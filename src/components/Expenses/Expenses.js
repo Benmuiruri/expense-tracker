93f078318dfdefd97dbdb4235f0bdfdb3ff602b6
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-26 09:31:13 +0300

    Add expenses bar indicator

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2022');
@@ -21,6 +22,7 @@ const Expenses = (props) => {
           selectedYear={filteredYear}
           onChangeYearFilter={yearFilterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList expenses={filteredExpenses} />
       </Card>
     </li>
